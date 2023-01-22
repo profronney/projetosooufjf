57848ad025ad988c26a155ff17e60c93ed89dd2f
profronney <75810735+profronney@users.noreply.github.com>
2023-01-21 21:40:45 -0300

    Update Triangulo.java
    Alteração

--- ExemplosJava/src/br/ufjf/projetosooufjf/exemplos/Triangulo.java
+++ ExemplosJava/src/br/ufjf/projetosooufjf/exemplos/Triangulo.java
@@ -6,14 +6,14 @@ public class Triangulo {
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in); //Leitor do teclado
-	    int altura, base; //Dados de entrada
-	    float area; //Dados de saida
-	    System.out.print("Informe a altura: ");
-	    altura = teclado.nextInt();
-	    System.out.print("Informe a base: ");
-	    base = teclado.nextInt();
-	    area = 0.5f * altura * base;
-	    System.out.println("Área: " + area);
+	    	int altura, base; //Dados de entrada
+	    	float area; //Dados de saida
+	    	System.out.println("Informe a altura: ");
+	    	altura = teclado.nextInt();
+	    	System.out.println("Informe a base: ");
+	    	base = teclado.nextInt();
+	    	area = 0.5f * altura * base;
+	    	System.out.println("Área: " + area);
 	}
 
 }
