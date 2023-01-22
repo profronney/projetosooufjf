38d86052d0bd98ec864fc4d588edeee1f4c82880
ronney <ronney@localhost>
2023-01-21 21:39:20 -0300

    Commit inicial

---
+++ ExemplosJava/src/br/ufjf/projetosooufjf/exemplos/Triangulo.java
@@ -0,0 +1,19 @@
+package br.ufjf.projetosooufjf.exemplos;
+
+import java.util.Scanner;
+
+public class Triangulo {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in); //Leitor do teclado
+	    int altura, base; //Dados de entrada
+	    float area; //Dados de saida
+	    System.out.print("Informe a altura: ");
+	    altura = teclado.nextInt();
+	    System.out.print("Informe a base: ");
+	    base = teclado.nextInt();
+	    area = 0.5f * altura * base;
+	    System.out.println("Área: " + area);
+	}
+
+}
